68e7b66300a66dc9881d00d16591872efc8ac353
Rimba Jati Dwi Djatmiko <120359027+rimbajati@users.noreply.github.com>
2023-11-29 22:19:19 +0700

    Update TransaksiPeminjaman.java

--- TransaksiPeminjaman.java
+++ TransaksiPeminjaman.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tahap2pbo;
 
 import java.util.Date;
 
-/**
- *
- * @author audhia
- */
 public class TransaksiPeminjaman {
     public String id;
     public Date tanggalPeminjaman;
