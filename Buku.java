ad8947be2fe3c302cf9b0b8fefc4bd6e970b4cbd
Rimba Jati Dwi Djatmiko <120359027+rimbajati@users.noreply.github.com>
2023-11-29 22:17:42 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tahap2pbo;
 
-/**
- *
- * @author audhia
- */
 public class Buku {
     public String judulBuku;
     public String pengarang;
