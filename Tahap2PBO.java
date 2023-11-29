7d8e1793d6d391f0b24276d184ea80b38d19a8a5
Rimba Jati Dwi Djatmiko <120359027+rimbajati@users.noreply.github.com>
2023-11-29 22:18:56 +0700

    Update Tahap2PBO.java

--- Tahap2PBO.java
+++ Tahap2PBO.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tahap2pbo;
 
 import java.util.Date;
 import java.util.Scanner;
 
-/**
- *
- * @author audhia
- */
 public class Tahap2PBO {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
