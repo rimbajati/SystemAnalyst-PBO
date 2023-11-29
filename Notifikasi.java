89250d9036517916e75d656323bafb19fc393d97
Rimba Jati Dwi Djatmiko <120359027+rimbajati@users.noreply.github.com>
2023-11-29 22:18:30 +0700

    Update Notifikasi.java

--- Notifikasi.java
+++ Notifikasi.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tahap2pbo;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- *
- * @author audhia
- */
 public class Notifikasi {
     private String id;
     private Date tanggal;
