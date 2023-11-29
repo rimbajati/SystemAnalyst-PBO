252cd6a6673b20deb2c783f4e8c9d96eb60015b0
Rimba Jati Dwi Djatmiko <120359027+rimbajati@users.noreply.github.com>
2023-11-29 22:17:06 +0700

    Update AnggotaPerpustakaan.java

--- AnggotaPerpustakaan.java
+++ AnggotaPerpustakaan.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tahap2pbo;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/**
- *
- * @author audhia
- */
 public class AnggotaPerpustakaan {
     public String nama;
     public String nomorAnggota;
